be1f08342318ffcb0f5775126da884ad990c195d
Benjamin Delmée <Benjamin.Delmee@fr.ey.com>
2019-10-11 23:19:13 +0200

    Make the template generic

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,17 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    kwargs = {
+        # mandatory
+        'page_title': 'ACME Company | World leader of ABCD',
+        'company_name': 'ACME Company',
+        'headline': 'The Leading Company',
+        # optional
+        'lang': 'fr',
+        'page_description': 'ACME Company is the world leader in...',
+        'call_to_action': 'Try it for free',
+        'call_to_action_link': 'https://typeform.com',
+        'learn_more': 'Learn more',
+        'headline_sub': 'Why choosing someone else?'
+    }
+    return render_template('index.html', **kwargs)
