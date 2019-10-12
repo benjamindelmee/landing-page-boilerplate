fb3a465483378be72b90538449dd8967aaf588e4
Benjamin Delmée <Benjamin.Delmee@fr.ey.com>
2019-10-12 13:45:51 +0200

    refactoring: improve variable name and code genericity

--- app/routes.py
+++ app/routes.py
@@ -4,26 +4,44 @@ from app import app
 @app.route('/')
 def index():
     kwargs = {
-        # mandatory
-        'page_title': 'ACME Company | World leader of ABCD',
-        'company_name': 'ACME Company',
-        'headline': 'The Leading Company',
-        # optional
-        'lang': 'fr',
-        'page_description': 'ACME Company is the world leader in...',
-        'call_to_action': 'try it for free',
-        'call_to_action_link': 'https://typeform.com',
-        'learn_more': 'Learn more',
-        'headline_sub': 'Why choosing someone else?',
-        'intro_title': 'Our outstanding value proposition',
-        'intro_text': 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
-        'advantage_one_image': 'http://placehold.it/700x400',
-        'advantage_one_image_alt': '',
-        'advantage_one_title': 'advantage one',
-        'advantage_one_text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit aliquam aperiam nulla perferendis dolor nobis numquam, rem expedita, aliquid optio, alias illum eaque. Non magni, voluptates quae, necessitatibus unde temporibus.',
-        'advantage_two_image': 'http://placehold.it/700x400',
-        'advantage_two_image_alt': '',
-        'advantage_two_title': 'advantage two',
-        'advantage_two_text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit aliquam aperiam nulla perferendis dolor nobis numquam, rem expedita, aliquid optio, alias illum eaque. Non magni, voluptates quae, necessitatibus unde temporibus.',
+        'meta_lang': 'en',
+        'meta_title': 'Product ABCD by ACME',
+        'meta_description': '',
+
+        'company_name': 'ACME',
+        'btn_calltoaction_text': 'try it for free',
+        'btn_calltoaction_link': 'https://typeform.com',
+        'btn_learnmore_text': 'Learn more',
+        'btn_learnmore_link': '#section-1',
+
+        'headline': 'Our product name',
+        'headline_sub': 'The most advanced product in the world',
+
+        'sections': [
+            {
+                'order': '1',
+                'type': 'text',
+                'title': 'Our outstanding value proposition',
+                'paragraph': 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.'
+            },
+            {
+                'order': '2',
+                'type': 'double_cards',
+                'left_card_image': 'http://placehold.it/700x400',
+                'left_card_image_alt': '',
+                'left_card_title': 'advantage one',
+                'left_card_text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit aliquam aperiam nulla perferendis dolor nobis numquam, rem expedita, aliquid optio, alias illum eaque. Non magni, voluptates quae, necessitatibus unde temporibus.',
+                'right_card_image': 'http://placehold.it/700x400',
+                'right_card_image_alt': '',
+                'right_card_title': 'advantage two',
+                'right_card_text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit aliquam aperiam nulla perferendis dolor nobis numquam, rem expedita, aliquid optio, alias illum eaque. Non magni, voluptates quae, necessitatibus unde temporibus.'
+            },
+            # {
+            #     'order': '42',
+            #     'type': 'text',
+            #     ...
+            # }
+        ]
     }
+
     return render_template('index.html', **kwargs)
