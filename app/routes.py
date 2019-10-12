b0bbc4e19a8e5b566463ce0c8a083cc6bda04eea
Benjamin Delmée <Benjamin.Delmee@fr.ey.com>
2019-10-12 10:38:18 +0200

    add introduction text

--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,8 @@ def index():
         'call_to_action': 'Try it for free',
         'call_to_action_link': 'https://typeform.com',
         'learn_more': 'Learn more',
-        'headline_sub': 'Why choosing someone else?'
+        'headline_sub': 'Why choosing someone else?',
+        'intro_title': 'Our outstanding value proposition',
+        'intro_text': 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.'
     }
     return render_template('index.html', **kwargs)
